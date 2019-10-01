f8ca8b04b894960070695d2a0585f1c2014a5156
Nickolay Samovendyuk <samovnick@gmail.com>
2019-10-01 15:35:51 +0300

    feat: implemented js-edu task

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ module.exports = function getTimeForEducation(
     knowsProgramming = true,
     config = {family: 4}
     ) {
-      return 0;
+      if (knowsProgramming) return Math.ceil(800 / config[focus]);
+      return Math.ceil(1300 / config[focus]);
   };
   
